d33c52c2eeb6399cf39f383356e86bf96372429c
XIAOMINGMMM1 <whm353487587@qq.com>
2019-04-20 23:38:45 +0800

    1111

---
+++ git-text/src/wtu/sj/Demo1.java
@@ -0,0 +1,13 @@
+package wtu.sj;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("1111");
+
+        System.out.println("1111");
+        System.out.println("1111");
+
+
+    }
+}
